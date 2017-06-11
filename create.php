97fc9a6561f6be00f0c404c13e3c2094e275a748
Tafelglotzer <nico.rickenbach@gmail.com>
2017-06-11 12:33:36 +0200

    Fix initialisation

--- create.php
+++ create.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Creator;
+use Creator\Creator;
+use Creator\Formatter\ArcturusFormatter;
 
 require 'vendor/autoload.php';
 
 $creator = new Creator();
 
-$creator->pushFormatter(new Formatter\ArcturusFormatter());
+$creator->pushFormatter(new ArcturusFormatter());
 
 $creator->run('arcturus', $_POST);
