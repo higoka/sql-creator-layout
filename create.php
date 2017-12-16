281ba39c5f7c68aa486a2e2769df5cd665546eb9
tafelglotzer <nico.rickenbach@gmail.com>
2017-12-16 20:55:32 +0100

    Added Comet Formatter

--- create.php
+++ create.php
@@ -1,14 +1,16 @@
 <?php
 
 use Creator\Application;
-use Creator\Formatter\PlusFormatter;
 use Creator\Formatter\ArcturusFormatter;
+use Creator\Formatter\PlusFormatter;
+use Creator\Formatter\CometFormatter;
 
 require 'vendor/autoload.php';
 
 $creator = new Application();
 
-$creator->pushFormatter(new PlusFormatter());
 $creator->pushFormatter(new ArcturusFormatter());
+$creator->pushFormatter(new PlusFormatter());
+$creator->pushFormatter(new CometFormatter());
 
 $creator->run($_POST['formatter'], $_POST);
